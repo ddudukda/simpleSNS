64c84a0c12619e8b87090806ca7e4cd0e28ffab7
ddudukda <dk.kwon094@gmail.com>
2022-09-28 14:13:54 +0900

    로그인 성공시 토큰을 이용하여 username 가져오기 위한 jwt토큰 설정

--- src/main/java/com/example/sns/exception/ErrorCode.java
+++ src/main/java/com/example/sns/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED,"Password is invalid"),
 
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED,"Token is invalid"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "internal server error")
     ;
 
