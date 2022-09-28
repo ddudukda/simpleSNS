70a0eb5297e74bec7c8494959d5f4ae0fae17445
ddudukda <dk.kwon094@gmail.com>
2022-09-28 22:27:36 +0900

    post list, mylist

--- src/main/java/com/example/sns/controller/PostController.java
+++ src/main/java/com/example/sns/controller/PostController.java
@@ -7,6 +7,8 @@ import com.example.sns.model.Post;
 import com.example.sns.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,5 +38,14 @@ public class PostController {
         return Response.success();
     }
 
+    @GetMapping
+    public Response<Page<PostResponse>> list(Pageable pageable, Authentication authentication) {
+        return Response.success(postService.list(pageable).map(PostResponse::fromPost));
+    }
+
+    @GetMapping("/my")
+    public Response<Page<PostResponse>> myPosts(Pageable pageable, Authentication authentication) {
+        return Response.success(postService.myPosts(authentication.getName(),pageable).map(PostResponse::fromPost));
+    }
 
 }
--- src/test/java/com/example/sns/service/PostServiceTest.java
+++ src/test/java/com/example/sns/service/PostServiceTest.java
@@ -13,6 +13,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
@@ -108,7 +110,6 @@ public class PostServiceTest {
         Assertions.assertEquals(ErrorCode.INVALID_PERMISSION, e.getErrorCode());
     }
 
-    ////
     @Test
     void 포스트삭제_성공한_경우() {
         String userName = "userName";
@@ -155,5 +156,20 @@ public class PostServiceTest {
         Assertions.assertEquals(ErrorCode.INVALID_PERMISSION, e.getErrorCode());
     }
 
+    @Test
+    void 피드목록_요청이_성공한_경우() {
+        Pageable pageable = mock(Pageable.class);
+        when(postEntityRepository.findAll(pageable)).thenReturn(Page.empty());
+        Assertions.assertDoesNotThrow(() -> postService.list(pageable));
+    }
+
+    @Test
+    void 내_피드목록_요청이_성공한_경우() {
+        Pageable pageable = mock(Pageable.class);
+        UserEntity user = mock(UserEntity.class);
+        when(userEntityRepository.findByUserName(any())).thenReturn(Optional.of(user));
+        when(postEntityRepository.findAllByUser(user, pageable)).thenReturn(Page.empty());
+        Assertions.assertDoesNotThrow(() -> postService.myPosts("", pageable));
+    }
 
 }
